cfb1b41abe6e68b6048b0a59d6856fa6ce402558
adruuuu9 <a.rodriguez@santmarc.com>
2023-05-16 18:59:24 +0200

    Waypoints and destroy vehicle

--- WarTraveler_VR/Assets/WarTraveler/Adri/Scripts/DestroyVehicle.cs
+++ WarTraveler_VR/Assets/WarTraveler/Adri/Scripts/DestroyVehicle.cs
@@ -1,21 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DestroyVehicle : MonoBehaviour
 {
-
-    public GameObject _DestroyVehicle;
-    
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Finish"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
