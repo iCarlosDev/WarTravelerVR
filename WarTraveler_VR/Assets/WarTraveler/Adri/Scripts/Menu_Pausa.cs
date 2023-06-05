dcf5724ffaa1ebffba38ab7c4309020834652a40
adruuuu9 <a.rodriguez@santmarc.com>
2023-06-05 11:34:08 +0200

    Pausamenu

--- WarTraveler_VR/Assets/WarTraveler/Adri/Scripts/Menu_Pausa.cs
+++ WarTraveler_VR/Assets/WarTraveler/Adri/Scripts/Menu_Pausa.cs
@@ -1,14 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Menu_Pausa : MonoBehaviour
 {
     public GameObject QuitGame_bttn;
     public GameObject ReturnLobby_bttn;
     public GameObject ResumeGame_bttn;
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        
+    }
     
-    
-    
+    public void ReturnLobby()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
     
 }
